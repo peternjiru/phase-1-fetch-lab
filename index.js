d54030a2796fc67cc4518046b554ae0b558abdd0
Peter Njiru <peternjirum@gmail.com>
2023-04-16 23:11:58 -0400

    completed fetch lab

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json())
+    .then(data => renderBooks(data))
+
 }
 
 function renderBooks(books) {
+
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
@@ -12,6 +16,6 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+document.addEventListener('DOMContentLoaded', function () {
+  fetchBooks()
 });
